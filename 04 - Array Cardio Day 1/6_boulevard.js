5831306a39e5d54786e8ebe931e0f0bcc9cc61b0
luisito0028 <gianluistapia28@gmail.com>
2025-11-13 23:29:41 +0800

    Answered 1 to 5

---
+++ 04 - Array Cardio Day 1/6_boulevard.js
@@ -0,0 +1,5 @@
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+
+//imma skip for now. its just using DOM stuff on that.. im lazy.
